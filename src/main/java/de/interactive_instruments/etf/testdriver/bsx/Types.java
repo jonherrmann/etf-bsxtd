abf509fa97de5ad9ec9834ce628d3fe9d0bf9bcc
Jon Herrmann <herrmann@interactive-instruments.de>
2017-05-10 15:09:06 +0200

    Refactor TestObjecTypeDetector

--- src/main/java/de/interactive_instruments/etf/testdriver/bsx/Types.java
+++ src/main/java/de/interactive_instruments/etf/testdriver/bsx/Types.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2016 interactive instruments GmbH
+ * Copyright 2010-2017 interactive instruments GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package de.interactive_instruments.etf.testdriver.bsx;
 
 import de.interactive_instruments.etf.dal.dto.capabilities.TestObjectTypeDto;
 import de.interactive_instruments.etf.dal.dto.test.TestItemTypeDto;
+import de.interactive_instruments.etf.detector.TestObjectTypeDetectorManager;
 import de.interactive_instruments.etf.model.DefaultEidMap;
 import de.interactive_instruments.etf.model.EidFactory;
 import de.interactive_instruments.etf.model.EidMap;
@@ -29,17 +30,8 @@ class Types {
 	private Types() {}
 
 	// Supported Test Object Types
-	public static final EidMap<TestObjectTypeDto> TEST_OBJECT_TYPES = new DefaultEidMap<TestObjectTypeDto>() {
-		{
-			final TestObjectTypeDto testObjectTypeDto = new TestObjectTypeDto();
-			testObjectTypeDto.setLabel("INSPIRE data set in GML");
-			testObjectTypeDto.setId(EidFactory.getDefault().createAndPreserveStr("e1d4a306-7a78-4a3b-ae2d-cf5f0810853e"));
-			testObjectTypeDto.setDescription("A set of XML documents. "
-					+ "Each document is either a WFS 2.0 FeatureCollection, a GML 3.2 Feature Collection "
-					+ "or an INSPIRE Base 3.2 or 3.3 SpatialDataSet. All features are GML 3.2 Features.");
-			put(testObjectTypeDto.getId(), testObjectTypeDto);
-		}
-	};
+	public static final EidMap<TestObjectTypeDto> BSX_SUPPORTED_TEST_OBJECT_TYPES = TestObjectTypeDetectorManager
+			.getTypes("e1d4a306-7a78-4a3b-ae2d-cf5f0810853e");
 
 	// Supported Test Item Types
 	public static final EidMap<TestItemTypeDto> TEST_ITEM_TYPES = new DefaultEidMap<TestItemTypeDto>() {
@@ -49,31 +41,38 @@ class Types {
 				testItemTypeDto.setLabel("Test Data Query Test Step");
 				testItemTypeDto.setId(EidFactory.getDefault().createAndPreserveStr("f483e8e8-06b9-4900-ab36-adad0d7f22f0"));
 				testItemTypeDto.setDescription("TODO description");
-				testItemTypeDto.setReference("https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Step-Types#teststeptype1");
+				testItemTypeDto.setReference(
+						"https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Step-Types#teststeptype1");
 				put(testItemTypeDto.getId(), testItemTypeDto);
 			}
 			{
 				final TestItemTypeDto testItemTypeDto = new TestItemTypeDto();
 				testItemTypeDto.setLabel("XQuery Test Assertion");
 				testItemTypeDto.setId(EidFactory.getDefault().createAndPreserveStr("f0edc596-49d2-48d6-a1a1-1ac581dcde0a"));
-				testItemTypeDto.setDescription("In an XQuery Test Assertion an XQuery expression is used to select XML from a data source that should be validated against an expected value");
-				testItemTypeDto.setReference("https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-1");
+				testItemTypeDto.setDescription(
+						"In an XQuery Test Assertion an XQuery expression is used to select XML from a data source that should be validated against an expected value");
+				testItemTypeDto.setReference(
+						"https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-1");
 				put(testItemTypeDto.getId(), testItemTypeDto);
 			}
+			// TODO move to SPI
 			{
 				final TestItemTypeDto testItemTypeDto = new TestItemTypeDto();
 				testItemTypeDto.setLabel("Manual Test Assertion");
 				testItemTypeDto.setId(EidFactory.getDefault().createAndPreserveStr("b48eeaa3-6a74-414a-879c-1dc708017e11"));
 				testItemTypeDto.setDescription("A Manual Test Assertion requires that a tester manually validates a result");
-				testItemTypeDto.setReference("https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-2");
+				testItemTypeDto.setReference(
+						"https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-2");
 				put(testItemTypeDto.getId(), testItemTypeDto);
 			}
+			// TODO move to SPI
 			{
 				final TestItemTypeDto testItemTypeDto = new TestItemTypeDto();
 				testItemTypeDto.setLabel("Disabled Test Assertion");
 				testItemTypeDto.setId(EidFactory.getDefault().createAndPreserveStr("92f22a19-2ec2-43f0-8971-c2da3eaafcd2"));
 				testItemTypeDto.setDescription("");
-				testItemTypeDto.setReference("https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-4");
+				testItemTypeDto.setReference(
+						"https://github.com/interactive-instruments/etf-bsxtd/wiki/Test-Assertion-Types#test-assertion-type-4");
 				put(testItemTypeDto.getId(), testItemTypeDto);
 			}
 
